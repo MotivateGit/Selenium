23abbd8235d44222efb79b97b00fa1eda1334fae
Bharathiraja Pichaimuthu <bharathiraja.pichaimuthu@sephora.com>
2024-09-30 16:12:34 +0530

    Updated the code

--- src/test/java/selenium/ActionsClassDemo.java
+++ src/test/java/selenium/ActionsClassDemo.java
@@ -1,8 +1,6 @@
 package selenium;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
